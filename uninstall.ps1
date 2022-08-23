05d59582c85122eea2d8589467a45b86679177cb
June Castillote <june.castillote@gmail.com>
2022-08-24 02:15:53 +0800

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -3,13 +3,32 @@ param (
     [string]$ModulePath
 )
 $moduleManifest = Test-ModuleManifest -Path $((Get-ChildItem -Path $PSScriptRoot -Filter *.psd1).FullName)
-$module = @(Get-Module ($moduleManifest.Name) -ListAvailable | Where-Object { $_.Version -eq $moduleManifest.Version })
+$module = @(Get-Module ($moduleManifest.Name) -ListAvailable) #| Where-Object { $_.Version -eq $moduleManifest.Version })
 
 if ($module.Count -gt 0) {
     foreach ($Moduleinfo in $module) {
         $ModulePath = $Moduleinfo.ModuleBase
         try {
-            Remove-Item -Path $ModulePath -Recurse -Force -Confirm:$false -ErrorAction Stop
+
+            $items = Get-ChildItem $ModulePath -Recurse
+            # Delete all module files
+            foreach ($file in (($items | Where-Object {!$_.PSIsContainer}).FullName)) {
+                # $file
+                [System.IO.File]::Delete($file)
+            }
+
+            # Delete all module subfolders
+            $folders = @(($items | Where-Object {$_.PSIsContainer}).FullName)
+            [array]::Reverse($folders)
+            foreach ($folder in $folders) {
+                [System.IO.Directory]::Delete($folder)
+            }
+
+            # Delete module folder and parent folder
+            [System.IO.Directory]::Delete($ModulePath)
+            [System.IO.Directory]::Delete($(Split-Path $ModulePath))
+
+            # Remove-Item -Path $ModulePath -Recurse -Force -Confirm:$false -ErrorAction Stop
             Write-Output "Done uninstalling $($Moduleinfo.Name) version $($Moduleinfo.Version) from $($Moduleinfo.ModuleBase)"
         }
         catch {
@@ -20,4 +39,7 @@ if ($module.Count -gt 0) {
             return $null
         }
     }
+}
+else {
+    "[$($moduleManifest.Name)] module not found. Nothing to uninstall." | Out-Default
 }
